941f138b3156529c4def76afcadb8da23e38b25d
kingaboros <71248858+kingaboros@users.noreply.github.com>
2020-12-11 16:46:39 +0000

    Fixed the bootstrap columns

--- src/script.js
+++ src/script.js
@@ -66,7 +66,7 @@ function displayForecast(response) {
   for (let index = 0; index < 6; index++) {
     forecast = response.data.list[index];
     forecastElement.innerHTML += `
-    <div class="col-2">
+    <div class="col-md-2">
     <h3>${formatHours(forecast.dt * 1000)}</h3>
     <img src="http://openweathermap.org/img/wn/${
       forecast.weather[0].icon
