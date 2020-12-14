ffe37f883234e3edc59b187899111bdeac40f82b
kingaboros <71248858+kingaboros@users.noreply.github.com>
2020-12-14 21:28:50 +0000

    Fixed apiUrl

--- src/script.js
+++ src/script.js
@@ -108,7 +108,7 @@ function searchLocation(position) {
 
   axios.get(apiUrl).then(displayWeather);
 
-  apiUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayForecast);
 }
